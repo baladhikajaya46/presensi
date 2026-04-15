c48805ac6e94301fd94aefc30dbac085ff2a2e9d
baladhikajaya46 <baladhikajaya46@gmail.com>
2026-04-15 20:14:57 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,3 +1,3 @@
 // Konfigurasi API Google Apps Script
 // Ganti URL ini dengan URL Web App dari Google Apps Script Anda (Setiap deploy akan ada URL baru)
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw7NRE9FBfUuAdpCaNBBZNoLwiVfUf09cF-yMhsqM8Atvm4nWt92cwkle5lOK8d9Q5g/exec'; 
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzStja_Au2V_rt9IGDsNjXVn6jLcBwMtQsA1aa_3wa31Vdzb5pgwPgr-NUNjzgUZzjV1A/exec'; 
